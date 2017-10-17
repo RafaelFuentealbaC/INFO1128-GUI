3d5508f39f8d40ca19ddd7e3fe704e7baf712d85
RafaelFuentealbaC <rafael.fuentealba2015@alu.uct.cl>
2017-10-17 16:29:50 -0300

    Ejercicio 5 Completado

--- Laboratorio #3/Ejercicio 5/Ejercicio 5.py
+++ Laboratorio #3/Ejercicio 5/Ejercicio 5.py
@@ -8,7 +8,17 @@ def HistoNormal(datos,archivo,img):
     plt.title('Histograma '+img+' Normal')
     plt.xlabel('Valores de intensidad')
     plt.ylabel('Numero de pixeles')
-    plt.savefig(archivo,bbox_inches='tight')
+    plt.savefig(archivo)
+    return None
+
+def HistoAcumulativo(datos,archivo,img):
+    plt.figure()
+    x = range(len(datos))
+    plt.bar(x,datos,align='center')
+    plt.title('Histograma '+img+' Acumulativo')
+    plt.xlabel('Valores de intensidad')
+    plt.ylabel('Numero de pixeles')
+    plt.savefig(archivo)
     return None
 
 aNomImg = ['F1','F2','F3','F4','F5','F6']
@@ -19,3 +29,15 @@ for i in aNomImg:
     htgNormal = imagen.histogram()
     HistoNormal(htgNormal,'Histogramas_'+i+'/Histo_Normal.PNG',i)
 print 'Histogramas Normales Guardados'
+
+for i in aNomImg:
+    imagen = Image.open('Histogramas_'+i+'/'+i+'.PNG')
+    if(imagen.mode!='L'): imagen = imagen.convert('L')
+    h_Acumulativo = []
+    acumulador = 0
+    htgAcumulativo = imagen.histogram()
+    for v in htgAcumulativo:
+        acumulador += v
+        h_Acumulativo.append(acumulador)
+    HistoAcumulativo(h_Acumulativo,'Histogramas_'+i+'/Histo_Acumulativo.PNG',i)
+print 'Histogramas Acumulativos Guardados'
